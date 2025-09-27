44aa3a02529fc35eca4fd76762996ce69af9863f
Ryona <danielgetahun46@gmail.com>
2025-09-27 13:21:45 -0700

    added code

--- wish.c
+++ wish.c
@@ -1,10 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <unistd.h>
 int  main(int argc, char* argv[])
 {
-char input[30];
- for(;;){
+char input[30]; 
+ do{
   printf("wish> ");
   scanf("%s", input);
- }
+  if (strcmp(input, "exit") == 0){
+    exit(0);
+  }
+  if (strcmp(input, "ls") == 0){
+    execv(input, argv[2]);
+    exit(0);
+  }
+ }while(true);
 }
