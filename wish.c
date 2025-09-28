a10f746ee26796f334399d51bee57b3814b7c2f5
Ryona <danielgetahun46@gmail.com>
2025-09-28 12:55:16 -0700

    added code

--- wish.c
+++ wish.c
@@ -1,20 +1,42 @@
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+
 int  main(int argc, char* argv[])
 {
-char input[30]; 
+char *input;
+size_t len = 0; 
+
  do{
   printf("wish> ");
-  scanf("%s", input);
+  ssize_t ninput =  getline(&input,&len,stdin);
   if (strcmp(input, "exit") == 0){
     exit(0);
   }
   if (strcmp(input, "ls") == 0){
-    execv(input, argv[2]);
-    exit(0);
+    pid_t pid = fork();
+    if (pid  == 0)
+    {
+      printf("child create\n");
+      char path[] = "/bin/ls";
+      char* argv[] = {"ls", NULL}; 
+      execv(path, argv);
+      perror("execv failed");
+      _exit(1);
+    }
+    else if (pid > 0)
+    {
+    int status;
+    waitpid(pid, &status, 0);
+    }
+    else
+    {
+    perror("fork failed");
+    exit(1);
+    }
   }
  }while(true);
 }
